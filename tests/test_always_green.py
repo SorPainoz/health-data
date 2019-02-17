c12a7abc7146b755bd17c67dfe2e6cb9eb99445a
igor_piroli <piroligor@gmail.com>
2019-02-17 08:50:51 +0100

    green, Lets start

--- tests/test_always_green.py
+++ tests/test_always_green.py
@@ -6,4 +6,4 @@ class AlwaysGreenTests(unittest.TestCase):
     def test_something(self):
         magic_number = 33
         self.assertEqual(33, magic_number)
-        self.assertTrue(False)
+        self.assertTrue(True)
